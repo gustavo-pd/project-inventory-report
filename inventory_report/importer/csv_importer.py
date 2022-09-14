f309f7ce4f5a237a5c3860a02ef07862354e41dc
GustavoPD <gudalmoro@gmail.com>
2022-09-14 05:25:01 -0300

    test

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -6,7 +6,9 @@ class CsvImporter(Importer):
     def import_data(path):
         if path.endswith('.csv'):
             with open(path, 'r') as file:
-                file_csv = list(csv.DictReader(file, delimiter=",", quotechar='"'))
+                file_csv = list(csv.DictReader(
+                    file, delimiter=",", quotechar='"'
+                ))
                 return file_csv
         else:
             raise ValueError("Arquivo inválido")
